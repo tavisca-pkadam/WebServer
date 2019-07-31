8ac22afb519005e1c779e64772d63633c2be74aa
Kadam <pakadam@internal.tavisca.com>
2019-07-31 21:46:30 +0530

    Add project files.

---
+++ WebServer/WebServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer
+{
+    class WebServer
+    {
+    }
+
+    class ServerListener
+    {
+
+    }
+
+    class WebPage
+    {
+
+    }
+    
+    class HttpUrl
+    {
+
+    }
+
+    class ServerRequest
+    {
+
+    }
+
+    class ServerResponse
+    {
+
+    }
+
+
+
+}
